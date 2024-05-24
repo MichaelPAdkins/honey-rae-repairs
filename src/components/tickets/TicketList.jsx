61724706e115a25fcf7a5af398b03158038093a8
Michael Adkins <michaelpauladkins@gmail.com>
2024-05-24 13:20:33 -0400

    currently working

--- src/components/tickets/TicketList.jsx
+++ src/components/tickets/TicketList.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { getAllTickets } from "../../../services/ticketService";
+import { getAllTickets } from "../../services/ticketService.jsx"
 import "./Tickets.css";
 
 export const TicketList = () => {
