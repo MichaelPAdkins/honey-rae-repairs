fa29b613aea083818ff9a96edd45dcd9bd19d0c3
Michael Adkins <michaelpauladkins@gmail.com>
2024-05-30 13:08:25 -0400

    last functional code from part 8

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import "./App.css"
+import { CustomerList } from "./components/customers/CustomersList.jsx";
 import { TicketList } from "./components/tickets/TicketList.jsx"
 
 export const App = () => {
   return ( 
   <>
-  <TicketList />
+  {/* <TicketList /> */}
+  <CustomerList />
   </>
   );
 };
